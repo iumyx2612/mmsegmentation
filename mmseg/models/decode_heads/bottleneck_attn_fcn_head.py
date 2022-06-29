efbd4292dc9fc9f2be4465c88c04d8b63caea9c3
iumyx2612 <deftnottroanh@gmail.com>
2022-06-29 08:24:24 +0700

    Add mid_channels feature in BAFCN

--- mmseg/models/decode_heads/bottleneck_attn_fcn_head.py
+++ mmseg/models/decode_heads/bottleneck_attn_fcn_head.py
@@ -12,6 +12,7 @@ class BAFCNHead(FCNHead):
     def __init__(self,
                  num_convs=2,
                  kernel_size=3,
+                 mid_channels=256,
                  concat_input=True,
                  dilation=1,
                  **kwargs):
@@ -19,6 +20,7 @@ class BAFCNHead(FCNHead):
         self.num_convs = num_convs
         self.concat_input = concat_input
         self.kernel_size = kernel_size
+        self.mid_channels = mid_channels
         super(BAFCNHead, self).__init__(num_convs,
                                           kernel_size,
                                           concat_input,
@@ -32,7 +34,7 @@ class BAFCNHead(FCNHead):
 
         self.bottleneck = ConvModule(
             self.in_channels,
-            self.channels,
+            self.mid_channels,
             kernel_size=1,
             padding=0,
             dilation=0,
@@ -44,7 +46,7 @@ class BAFCNHead(FCNHead):
         convs = []
         convs.append(
             ConvModule(
-                self.channels,
+                self.mid_channels,
                 self.channels,
                 kernel_size=kernel_size,
                 padding=conv_padding,
@@ -69,7 +71,7 @@ class BAFCNHead(FCNHead):
             self.convs = nn.Sequential(*convs)
         if self.concat_input:
             self.conv_cat = ConvModule(
-                self.channels + self.channels,
+                self.mid_channels + self.channels,
                 self.channels,
                 kernel_size=kernel_size,
                 padding=kernel_size // 2,
