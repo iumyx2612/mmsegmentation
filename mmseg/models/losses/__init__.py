794650525f84c46715ba762b4413222ee7e815fa
iumyx2612 <deftnottroanh@gmail.com>
2022-07-01 10:35:39 +0700

    Add FocalTversky Loss

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -5,6 +5,7 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
 from .dice_loss import DiceLoss
 from .focal_loss import FocalLoss
 from .lovasz_loss import LovaszLoss
+from .focal_tversky_loss import FocalTverskyLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 
 __all__ = [
